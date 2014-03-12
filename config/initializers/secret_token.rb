670bf24a415c226811e799f27e30d1f48f38f2c9
[Soraya Hossain] <[sshossain@gmail.com]>
2014-03-11 22:18:56 -0300

    Adding Devise and ActiveAdmin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Staunchy::Application.config.secret_key_base = '32e513e4eca7fbd9678fc05169378cd50af93bdde33ab0f930641c18ddc375879c943c2622d0191b7a4742ad69d694162f244ba4af4b571c64930f69930cfe4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Staunchy::Application.config.session_store :cookie_store, key: '_Staunchy_session'
