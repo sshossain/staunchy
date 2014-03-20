bec8e2ef4e880ebaec77bc87c0fafa66a989d863
[Soraya Hossain] <[sshossain@gmail.com]>
2014-03-20 18:16:35 -0300

    Add order by name to categories

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
 	def index
-  	@products = Product.on_sale.in_stock
+  	@products = Product.on_sale.in_stock.includes(:category).sort_by{|p| p.category.name}
 	end
 
 	def about
