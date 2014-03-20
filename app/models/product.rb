6ca9aaa7efb1ebe66e98f7f044982a0414b169ed
[Soraya Hossain] <[sshossain@gmail.com]>
2014-03-20 02:01:50 -0300

    Adding friendly URLs to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,8 @@ class Product < ActiveRecord::Base
   scope :on_sale, -> { where(on_sale: true) }
   scope :in_stock, -> { where("quantity > 0") }
 
+  def to_param
+  	[id, name.parameterize].join("-")
+  end
+
 end
